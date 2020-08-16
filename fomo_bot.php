ddad01afe2a5993a40b59d58d74ec35bc00ea0d3
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-08-16 13:38:18 +0000

    Add more code

--- fomo_bot.php
+++ fomo_bot.php
@@ -6,4 +6,11 @@ $botToken = "Your_bot_token_here";
 $website = "https://api.telegram.org/bot".$botToken;
  
 $update = file_get_contents('php://input');
-$update =
+$update = json_decode($update, TRUE);
+ 
+ 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+function url_get_contents ($Url) {
+    if (!function_exist
