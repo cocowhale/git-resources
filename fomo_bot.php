908b1125ec089e91a175af051d17df744f40e53f
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-08-20 17:20:04 +0000

    brackets

--- fomo_bot.php
+++ fomo_bot.php
@@ -19,3 +19,7 @@ function url_get_contents ($Url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $outpu
