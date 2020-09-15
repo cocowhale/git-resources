1e3713221d1bb5d3d4c3321051b075e36ee816f1
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-09-15 12:53:45 +0000

    anim

--- fomo_bot.php
+++ fomo_bot.php
@@ -152,4 +152,6 @@ case "/getchinaticker@FOMO_bot":
                 $huobiprice = $huobiarray['ticker']['last'];
 
 
-                $chinafetc
+                $chinafetch = file_get_contents('https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny');
+                $chinaarray = json_decode($chinafetch, true);
+                $c
