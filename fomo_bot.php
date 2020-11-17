2152bb6a4df80f7cb27a6390d487b635cbdb1c92
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-11-17 21:05:54 +0000

    Heroku setup

--- fomo_bot.php
+++ fomo_bot.php
@@ -415,4 +415,8 @@ case "/getmarginfunding@FOMO_bot":
                 $finexlongarray = json_decode($finexlong,true);
                 $finexusdmargltcusd = intval($finexlongarray[0][1]);
 
-                $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.
+                $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fUSD:tBFXUSD/hist');
+                $finexlongarray = json_decode($finexlong,true);
+                $finexusdmargbfxusd = intval($finexlongarray[0][1]);
+
+                $usdusedbt
