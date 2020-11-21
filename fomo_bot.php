9daae0647c93dcf5425706147cf0fa66156baa85
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-11-21 14:26:51 +0000

    Add eslintignore

--- fomo_bot.php
+++ fomo_bot.php
@@ -434,4 +434,7 @@ case "/getmarginfunding@FOMO_bot":
 
 
                 $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fBTC:tBTCUSD/hist');
-                $finexlongarray = json
+                $finexlongarray = json_decode($finexlong,true);
+                $finexbtcmargbtcusd = intval($finexlongarray[0][1]);
+
+                $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats
