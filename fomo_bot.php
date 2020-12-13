116d79373633bb512d7a7c78bb5ae59efda700b3
cocowhale <109966680+cocowhale@users.noreply.github.com>
2020-12-13 21:58:26 +0000

    fixed list

--- fomo_bot.php
+++ fomo_bot.php
@@ -526,4 +526,6 @@ case "/topminers@FOMO_bot":
 
                 #build individual message
                 if ($weightedpct < 0):
-                    $wsistring="<b>Whalepool Shitcoin Index (WSI
+                    $wsistring="<b>Whalepool Shitcoin Index (WSI 10)</b>\n<b>         ".number_format($wsivalue,"2")." pts (".number_format($weightedpct,"2")."%) </b>\n<code> Name Value(BTC) 24Hr Chg</code>\n";
+                else:
+         
