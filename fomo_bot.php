da1fc208e85819ed445b1c68db6795d953055924
cocowhale <109966680+cocowhale@users.noreply.github.com>
2021-01-10 16:35:14 +0000

    testing pre-commit hook

--- fomo_bot.php
+++ fomo_bot.php
@@ -622,4 +622,10 @@ case "/getswaprates@FOMO_bot":
                 $usdffrarray2 = json_decode($usdffrjson2, true);
                 if (isset($usdffrarray2)) {
                 $usdffr2 = round($usdffrarray2['bids'][0]['rate'],1);
-                $usdffr2d=round($usdffr
+                $usdffr2d=round($usdffr2/365,4);
+                } else {
+                $usdffr2 = "N/A";
+                }
+
+                $grabusdmarg = file_get_contents('https://api.bitfinex.com/v1/lends/usd');
+               
