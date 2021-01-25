482868150ae7ec905db7380bcb6b748f2d566654
cocowhale <109966680+cocowhale@users.noreply.github.com>
2021-01-25 18:45:11 +0000

    Update biosketch.md

--- fomo_bot.php
+++ fomo_bot.php
@@ -696,4 +696,8 @@ case "/getswaprates@FOMO_bot":
                 } else {
                 $etcffr2 = "N/A";
                 }
-                $grabetcmarg = file_get_contents('https://api.bitfinex.com/v1/lends/etc')
+                $grabetcmarg = file_get_contents('https://api.bitfinex.com/v1/lends/etc');
+                $etcmargarray = json_decode($grabetcmarg, true);
+                $theetcffr = $etcmargarray[0]['rate'];
+                $theetcffr1=round($theetcffr/365,4);
+ 
