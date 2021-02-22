2e990ac9c45cdc556a4c284d714c6ddc9962b0ab
cocowhale <109966680+cocowhale@users.noreply.github.com>
2021-02-22 21:27:30 +0000

    update GUI

--- fomo_bot.php
+++ fomo_bot.php
@@ -783,4 +783,7 @@ sendMessageTypingAction($chatId);
                 $okcbiwkarray = json_decode($okcbiweekly, true);
                 $okcbiwkprice = $okcbiwkarray['ticker']['last'];
                 $biwkpremium = round((($okcbiwkprice - $okcixprice)/$okcbiwkprice)*100,2);
-                $bip=round($okcb
+                $bip=round($okcbiwkprice - $okcixprice,2);
+
+                $okcqtly = file_get_contents('https://www.okcoin.com/api/v1/future_ticker.do?symbol=btc_usd&contract_type=quarter');
+                $okcqtarray = 
