f670c3a9898a53376b23d61e71c817b3fd1ac801
cocowhale <109966680+cocowhale@users.noreply.github.com>
2021-03-20 07:36:41 +0000

    Added simplecov

--- fomo_bot.php
+++ fomo_bot.php
@@ -805,4 +805,8 @@ sendMessageTypingAction($chatId);
                 $itbit = file_get_contents('https://api.itbit.com/v1/markets/XBTUSD/ticker');
                 $okcoin = file_get_contents('https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd');
                 $gemini = file_get_contents('https://api.gemini.com/v1/pubticker/btcusd');
-		$kraken = file_get_contents('https://a
+		$kraken = file_get_contents('https://api.kraken.com/0/public/Ticker?pair=XBTUSD');
+		$quoine = file_get_contents('https://api.quoine.com/products/');
+
+                $finexarray = json_decode($finex,true);
+                $stamparray = json_decode($stam
