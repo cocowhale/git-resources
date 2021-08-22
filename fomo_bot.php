1753a5fd96f20ceb5cb795d8c7d316684cf72c0f
cocowhale <109966680+cocowhale@users.noreply.github.com>
2021-08-22 12:35:02 +0000

    new pic

--- fomo_bot.php
+++ fomo_bot.php
@@ -1106,4 +1106,17 @@ sendMessageTypingAction($chatId);
 $cfbpijson = file_get_contents('https://www.cryptofacilities.com/derivatives/api/cfbpi');
 $cfbpiarray = json_decode($cfbpijson, true);
 if (isset($cfbpiarray)) {
-if ($cfbpiarray['result'] == "s
+if ($cfbpiarray['result'] == "success") {
+$cfbpi = $cfbpiarray['cf-bpi'];
+} else {
+$cfbpi = "0";
+return;
+} 
+} else {
+$cfbpi = "0";
+return;
+}
+$finexprice = $cfbpi;
+
+#$finexprice = $finexarray['last'];
+                $chinausd=
