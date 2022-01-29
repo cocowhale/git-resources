c595f3ddfb85cd46762c931962ecd2f6e68d6fb0
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-01-29 15:31:27 +0000

    FIXED

--- fomo_bot.php
+++ fomo_bot.php
@@ -1429,4 +1429,7 @@ sendMessageTypingAction($chatId);
 
                 $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fUSD:tXMRUSD/hist');
                 $finexlongarray = json_decode($finexlong,true);
-                $finexusdmargxmrusd = intval($finexlong
+                $finexusdmargxmrusd = intval($finexlongarray[0][1]);
+
+                $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fUSD:tDSHUSD/hist');
+                $finexlongarray = json_decode($f
