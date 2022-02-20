6d015d9859d5472e835a574abd5d1041dd08c735
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-02-20 11:45:06 +0000

    News

--- fomo_bot.php
+++ fomo_bot.php
@@ -1465,4 +1465,7 @@ sendMessageTypingAction($chatId);
 
                 $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fBTC:tLTCBTC/hist');
                 $finexlongarray = json_decode($finexlong,true);
-                $finexbtcmargltc
+                $finexbtcmargltcbtc = intval($finexlongarray[0][1]);
+
+                $finexlong=file_get_contents('https://api2.bitfinex.com:3000/api/v2/stats1/credits.size.sym:1m:fBTC:tZECBTC/hist');
+                $f
