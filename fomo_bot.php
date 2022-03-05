f70735c84f74bb75459b6ccae566a85ec42f1146
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-03-05 16:14:10 +0000

    support windows

--- fomo_bot.php
+++ fomo_bot.php
@@ -1495,4 +1495,4 @@ sendMessageTypingAction($chatId);
                 $btcmarglentusd=$btcmarglent*$finexprice;
                 $ratiolend=round($btcmarglentusd/$usdmarglent,2);
 
-                sendMessage($chatId, "<b>Bitfinex Margin Funding Statistics</b>\n<code>USD lent: </code>$".number_format($usdmarglent)."\n<code>USD used: </code>$".number_format($usdmargused
+                sendMessage($chatId, "<b>Bitfinex Margin Funding Statistics</b>\n<code>USD lent: </code>$".number_format($usdmarglent)."\n<code>USD used: </code>$".number_format($usdmargused)." (<b>".$usdusedperc."%</b>)\nBTC: ".number_format($usdusedbtcusdperc)."% ETH: ".number_format($usdusedethusdperc)."% ETC: ".number_format($usdusedetcusdperc)."% LTC: ".number_format($usdusedltcusdperc).
