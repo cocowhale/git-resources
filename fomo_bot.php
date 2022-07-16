a12dbb9821d65ac3864a78bf5e4e68842592fe16
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-07-16 12:49:14 +0000

    initial commit.

--- fomo_bot.php
+++ fomo_bot.php
@@ -1761,3 +1761,10 @@ $quoinearray = json_decode($quoine, true);
                 $btcevol = $btcearray['btc_usd']['vol_cur'];
                 $itbitvol = $itbitarray['volume24h'];
                 $okcoinvol = $okcoinarray['ticker']['vol'];
+                $geminivol = $geminiarray['volume']['BTC'];
+                $krakenvol = $krakenarray['result']['XXBTZUSD']['v'][1];
+
+$quoinevol = $quoinearray[0]['volume_24h'];
+
+
+        
