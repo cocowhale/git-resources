ab269a009b0f1b1b2db4cbeb71207c629d022c94
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-12-15 18:03:06 +0000

    Add const

--- fomo_bot.php
+++ fomo_bot.php
@@ -2048,4 +2048,5 @@ sendMessageTypingAction($chatId);
 
                 #build individual message
                 if ($weightedpct < 0):
-                    $wsistring="<b>
+                    $wsistring="<b>Whalepool Shitcoin Index (WSI 10)</b>\n<b>         ".number_format($wsivalue,"2")." pts (".number_format($weightedpct,"2")."%) </b>\n<code> Name Value(BTC) 24Hr Chg</code>\n";
+        
