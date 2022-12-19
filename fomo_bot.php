eed7e419b4f65ad44718dd72e22bd45455ad1822
cocowhale <109966680+cocowhale@users.noreply.github.com>
2022-12-19 15:41:44 +0000

    Updated build files

--- fomo_bot.php
+++ fomo_bot.php
@@ -2064,4 +2064,5 @@ sendMessageTypingAction($chatId);
                     #$wsistring=$wsistring.$wsirank.". <code>".$wsi[$x]['symbol']."</code>: ".number_format($wsi[$x]['price_btc'],8)." (".number_format($wsi[$x]['percent_change_24h'],2)."%)\n";
                     $wsistring=$wsistring.$i.". <code>".$wsi[$x]['symbol']."</code>: ".number_format($poloprice,8)." (".number_format($polopricechg,2)."%)\n";
                 else:
-                    #$wsistring=$wsistring
+                    #$wsistring=$wsistring.$wsirank.". <code>".$wsi[$x]['symbol']."</code>: ".number_format($wsi[$x]['price_btc'],8)." (+".number_format($wsi[$x]['percent_change_24h'],2)."%)\n";
+           
